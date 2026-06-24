41af2d9a0f782d14d949f2c7ee78f8bd80e2bda0
Tomas Barisone <tomasbarisone16@gmail.com>
2026-06-24 01:03:09 -0300

    Cambio en la IP de NetworkConfig y agregado de instrucciones en el README del backend

--- app/src/main/java/com/example/apk_mock/data/remote/NetworkConfig.kt
+++ app/src/main/java/com/example/apk_mock/data/remote/NetworkConfig.kt
@@ -1,5 +1,8 @@
 package com.example.apk_mock.data.remote
 
 object NetworkConfig {
-    const val DEFAULT_BASE_URL = "http://192.168.1.212:8080/"
+    const val DEFAULT_BASE_URL = "http://10.0.2.2:8080/"
+
+    // En caso de usar el celular
+    // const val DEFAULT_BASE_URL = "http://IP_DE_LA_PC:8080/"
 }
